4364b769d2979deea79b243960d9bd93a51f20b2
jessicacunalata29 <jessicacunalata29@gmail.com>
2025-11-06 19:30:51 -0500

    Finalizado

--- src/app/auth/auth-module.ts
+++ src/app/auth/auth-module.ts
@@ -5,9 +5,14 @@ import { AuthRoutingModule } from './auth-routing-module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
+
+  providers:[
+    AuthService
+  ],
   declarations: [
     
   ],
@@ -16,7 +21,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     AuthRoutingModule,
     ReactiveFormsModule,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HttpClientModule,
+    
   ]
 })
 export class AuthModule { }
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup,  ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup,  ReactiveFormsModule , Validators} from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +14,25 @@ import { JsonPipe } from '@angular/common';
   ]
 })
 export class LoginComponent {
-  loginForm=new FormGroup({
-    email:new FormControl('admin@hotmail.com'),
-    password:new FormControl('admin1234'),
-  })
 
-  funIngresar(){
-    alert('Ingresando...');
+ private authService = inject(AuthService);
+
+
+  loginForm=new FormGroup({
+    email:new FormControl("",[Validators.email,Validators.required]),
+    password:new FormControl("",Validators.required),
+  })
+ funIngresar() {
+    this.authService.loginConNest(this.loginForm.value).subscribe({
+      next: (res) => {
+        console.log("✅ Login correcto:", res);
+      },
+      error: (err) => {
+        console.error("❌ Error en login:", err);
+      }
+    });
+  }
 }
-}
+    //alert('Ingresando...');
+
+
