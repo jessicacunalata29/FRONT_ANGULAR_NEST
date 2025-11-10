1bed62a9e4e3d59ed0a3130fa77dbd735f1fd11b
jessicacunalata29 <jessicacunalata29@gmail.com>
2025-11-09 20:07:43 -0500

    21

--- src/app/admin/admin-module.ts
+++ src/app/admin/admin-module.ts
@@ -5,6 +5,7 @@ import { PerfilComponent } from './components/perfil/perfil.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { RouterModule } from '@angular/router'; 
 import { LayoutComponent } from './layout/layout.component';
+import { AppLayoutModule } from './layout/app.layout.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { LayoutComponent } from './layout/layout.component';
     CommonModule,
     AdminRoutingModule,
      RouterModule, 
-    PerfilComponent,  // ✅ este SÍ es standalone
+    PerfilComponent,
+    AppLayoutModule  // ✅ este SÍ es standalone
   ]
 })
 export class AdminModule {}
